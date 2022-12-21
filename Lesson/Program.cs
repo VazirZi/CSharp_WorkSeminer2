f390b0a7612d4efc7c3f0c36e17455d381f5d4fa
VazirZi <vazir.zivar@mail.com>
2022-12-21 21:07:39 +0500

    Задача № 3

--- Lesson/Program.cs
+++ Lesson/Program.cs
@@ -28,6 +28,7 @@ Console.WriteLine($"Вторая цифра числа {num} = {res}");
 
 //=========================Задача_2=========================
 
+/*
 int thridNumber (int n)     // функция для получения третьей цифры введенного числа
 {
     int d = 0;
@@ -53,3 +54,23 @@ int res = thridNumber(number);  // вызываем функцию для пол
 
 Console.WriteLine($"Третья цифра числа {number} = {res}");
 }
+*/
+
+//=========================Задача_3=========================
+
+/*
+Console.Write("Введите число дня недели (1 = понедельник и т. д.): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+switch (number)
+{
+    case 1 : Console.WriteLine($"Не выходной (понедельник ({number}))"); break;
+    case 2 : Console.WriteLine($"Не выходной (вторник ({number}))"); break;
+    case 3 : Console.WriteLine($"Не выходной (среда ({number}))"); break;
+    case 4 : Console.WriteLine($"Не выходной (четверг ({number}))"); break;
+    case 5 : Console.WriteLine($"Не выходной (пятница ({number}))"); break;
+    case 6 : Console.WriteLine($"Выходной (суббота ({number}))"); break;
+    case 7 : Console.WriteLine($"Выходной (воскресенье ({number}))"); break;
+    default : Console.WriteLine("Нет такого дня"); break;
+}
+*/
